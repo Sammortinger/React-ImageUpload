118e56670c7522a2438ad7775d685dc80e568e73
Sam Mortinger <sam@viromodo.com>
2023-05-30 14:49:39 -0400

    Added edit & save button, ability to clear image, and polished look & feel of component

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -7,9 +7,8 @@ const ImageUpload = (props: any) => {
 
     const [fileInput, setFileInput] = useState<any>();
     const [hasInput, setHasInput] = useState(false);
-    const [equalAspect, setEqualAspect] = useState(false);
-    const [round, setRound] = useState(false);
     const [croppedImage, setCroppedImage] = useState<any>();
+    const [fileName, setFileName] = useState<string>("");
     
 
     const dialogRef = useRef<HTMLDialogElement>(null);
@@ -33,11 +32,57 @@ const ImageUpload = (props: any) => {
         const file = e.currentTarget.files[0];
 
         console.log(file);
+        setFileName(file.name);
         getBase64(file);
     }
 
+    const clearFileInput = () => {
+        setHasInput(false);
+        setFileInput(null);
+        setCroppedImage(null);
+        setFileName("");
+    }
+
+    const saveImage = () => {
+        props.setCroppedImage(croppedImage);
+        props.setOriginalImage(fileInput);
+    }
+
+    const dropHandler =(ev: any) => {
+        console.log("File(s) dropped");
+        
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+        
+        if (ev.dataTransfer.items) {
+            // Use DataTransferItemList interface to access the file(s)
+            [...ev.dataTransfer.items].forEach((item, i) => {
+            if (item.kind === "file" && (item.type === "image/png" || item.type === "image/gif" || item.type === "image/jpg" || item.type === "image/jpeg")) {
+                const file = item.getAsFile();
+                console.log(`… file[${i}].name = ${file.name}`);
+                setFileName(file.name);
+                getBase64(file);
+            }
+            });
+        } else {
+            // Use DataTransfer interface to access the file(s)
+            [...ev.dataTransfer.files].forEach((file, i) => {
+                console.log(`… file[${i}].name = ${file.name}`);
+            });
+        }
+    }
+
+    const dragOverHandler = (ev: any) => {
+        console.log("File(s) in drop zone");
+      
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+    }
+      
+
     useEffect(() => {
         setHasInput(fileInput !== null);
+        console.log(fileInput);
         showEditor();
     }, [fileInput])
 
@@ -57,11 +102,19 @@ const ImageUpload = (props: any) => {
         <div>
             {croppedImage && 
             <div id="img-display">
+                <div id="clear-button" onClick={() => {clearFileInput()}}>𐌢</div>
                 <img id={props.round ? "round" : ""} width={props.width || 250} src={croppedImage} />
-                <button onClick={showEditor}>Edit</button>
+                <div id="options-row">
+                    <button id="edit-button" onClick={showEditor}>Edit</button>
+                    <button id="save-button" style={{backgroundColor: props.color ? props.color : "dodgerblue"}} onClick={saveImage}>Save</button>
+                </div>
             </div>
             }
-            <input id="image-input" type="file" accept=".png,.jpg,.jpeg,.gif" onInput={(e) => {handleFile(e)}} />
+            <div id="drop-zone" style={{borderColor: props.color ? props.color : "dodgerblue"}} onDrop={() => dropHandler(event)} onDragOver={() => dragOverHandler(event)}>
+                <p id="drop-label">Click or drag a file to <i>upload</i>.</p>
+                <input id="image-input" type="file" accept=".png,.jpg,.jpeg,.gif" onInput={(e) => {handleFile(e)}} />
+                {fileInput && <p id="file-name">{fileName}</p>}
+            </div>
             <dialog ref={dialogRef} id="editor">
                 <div id={props.round ? "round" : "rect"}>
                     <Cropper
@@ -73,7 +126,7 @@ const ImageUpload = (props: any) => {
                         ref={cropperRef}
                     />
                 </div>
-                <button onClick={onCrop}>Save</button>
+                <button id="crop-button" onClick={onCrop}>Crop</button>
             </dialog>
         </div>
      );
